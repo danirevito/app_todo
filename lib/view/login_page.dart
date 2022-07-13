659870ed0ae2a3a0f74e22a2c365c45f8ccb066a
Daniela Cristina <danielacristina@MacBook-Air-de-Daniela.local>
2022-07-13 20:21:25 -0300

    tela de login e cadastro criada

---
+++ lib/view/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class loginpage extends StatefulWidget {
+  const loginpage({Key? key}) : super(key: key);
+
+  @override
+  State<loginpage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.purple
+    );
+  }
+}
