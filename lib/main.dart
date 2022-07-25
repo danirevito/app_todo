53784ec516fd7ec0c0b7faaf5f191bd2ba237c03
SoyRafaCosta <rafael.costa.rc.2020@gmail.com>
2022-07-25 20:03:30 -0300

    salvando novas atualizações

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app_todo/modules/home/home_view.dart';
+import 'package:app_todo/view/novatarefa.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ListagemTarefas(),
+      home: const NovaTarefa(),
     );
   }
 }
