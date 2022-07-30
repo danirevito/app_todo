ac26927497442553ba0fd5ea1bfd7581a92a8588
SoyRafaCosta <rafael.costa.rc.2020@gmail.com>
2022-07-30 15:00:01 -0300

    pagina da Pyetra ok

--- lib/modules/cadastre/cadastre_sucess.dart
+++ lib/modules/cadastre/cadastre_sucess.dart
@@ -7,77 +7,85 @@ class CadastreSucess extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 152, 36, 172),
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: 500,
-            height: 300,
-            padding: const EdgeInsets.all(5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "Cadastro concluído!",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Começar", style: TextStyle(fontSize: 18)),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)),
-                    side: BorderSide(color: Colors.white),
-                    primary: Color.fromARGB(255, 12, 7, 155),
-                    padding: EdgeInsets.symmetric(vertical: 18, horizontal: 24),
-                    textStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
+      backgroundColor: const Color.fromARGB(255, 152, 36, 172),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 200,
             ),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-          Container(
-            width: 500,
-            height: 300,
-            //width: double.infinity,
-            decoration: BoxDecoration(
+            const Text(
+              "Cadastro concluído!",
+              style: TextStyle(
                 color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(195),
-                  topRight: Radius.circular(195),
-                )),
-            child: Column(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/homepage');
+              },
+              child: const Text(
+                "Começar",
+                style: TextStyle(fontSize: 18),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                side: const BorderSide(color: Colors.white),
+                primary: const Color.fromARGB(255, 12, 7, 155),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 18, horizontal: 24),
+                textStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 252,
+            ),
+            Stack(
               children: [
                 Container(
-                  //alignment: AlignmentDirectional.topCenter,
-                  child: Image.asset(
-                    "images/voacoruja.png",
-                    width: 90,
-                    height: 90,
-                    //fit: BoxFit.cover,
+                  width: 500,
+                  height: 250,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(195),
+                        topRight: Radius.circular(195),
+                      )),
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: AlignmentDirectional.topCenter,
+                        child: Image.asset(
+                          "images/voacoruja.png",
+                          width: 220,
+                          height: 220,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const Text(
+                        //alignment: Alignment.topCenter,
+                        "Os ventos da programação estão indo até você",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 152, 36, 172),
+                            fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-                ),
-                Text(
-                  "Os ventos da programação estão indo até você",
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 2, 4, 71), fontSize: 16),
-                  textAlign: TextAlign.center,
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
